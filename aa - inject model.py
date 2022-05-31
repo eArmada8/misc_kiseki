ba5147d61d7b3346547408cd8a27863dbcbd8c51
eArmada8 <103985972+eArmada8@users.noreply.github.com>
2022-05-30 22:28:02 -0700

    Add handling for xml file size change
    The script can now update the file location offsets if the XML file size changes due to changing the filename of the package.

--- aa - inject model.py
+++ aa - inject model.py
@@ -31,18 +31,22 @@ if __name__ == "__main__":
         input()
         raise SystemExit()
     if not sourcefile.split('_')[-1][0] == targetfile.split('_')[-1][0]:
+        # Note: This checks the first character of the last substring when the name is split by underscore.
+        # "C_CHR001_C02" will be type "C" and "C_CHR001_FC1" will be type "F"
+        # "C_CHR001" will be type "C" which, while not elegant, will work.
         print ('Error: Source and target files are not compatible types!  Press any key to quit.')
         input()
         raise SystemExit()
 
     # Generate the search and substitution strings to inject into the target model
     if sourcefile.split('_')[-1][0] == 'C':
-        source_string = sourcefile
-        target_string = targetfile
+        source_string = '<asset symbol="' + sourcefile + '">'
+        target_string = '<asset symbol="' + targetfile + '">'
     else:
-        source_string = sourcefile.split('_')[0] + '_' + sourcefile.split('_')[1]
-        target_string = targetfile.split('_')[0] + '_' + targetfile.split('_')[1]
-
+        source_string = '<asset symbol="' + sourcefile.split('_')[0] + '_' + sourcefile.split('_')[1] + '">'
+        target_string = '<asset symbol="' + targetfile.split('_')[0] + '_' + targetfile.split('_')[1] + '">'
+    offset_difference = len(target_string) - len(source_string)
+    
     # Make a target backup, only if no backup exists.
     if not os.path.exists(targetfile + '.pkg.original'):
         shutil.copy2(targetfile + '.pkg', targetfile + '.pkg.original')
@@ -52,7 +56,30 @@ if __name__ == "__main__":
         source_file_data = f.read()
 
     # Patch model with substitution string
-    patched_file_data = source_file_data.replace(bytes(source_string, 'ascii'),bytes(target_string, 'ascii'))
+    patched_file_data = bytearray(source_file_data.replace(bytes(source_string, 'ascii'),bytes(target_string, 'ascii')))
+
+    # If the filenames are of different length, the XML file size will have changed and the file offsets need to change
+    if not offset_difference == 0:
+        total_offsets = source_file_data[4]
+        file_offsets = []
+        # Read in all the current file offsets
+        for offset in range(total_offsets):
+            offset_location = (offset+1)*80
+            file_offsets.append(int.from_bytes(source_file_data[offset_location:offset_location+4], "little"))
+        # Calculate all the new file offsets
+        new_file_offsets = [i + offset_difference for i in file_offsets]
+        new_file_offsets[0] = file_offsets[0] # The first offset doesn't actually change
+        # Set the new file offsets into the patched file data
+        for offset in range(total_offsets):
+            offset_location = (offset+1)*80
+            offset_bytes = new_file_offsets[offset].to_bytes(4, 'little')
+            for intbyte in range(4):
+                patched_file_data[offset_location+intbyte] = offset_bytes[intbyte]
+        # Calculate and set the new XML file size
+        new_xml_size_bytes = (int.from_bytes(source_file_data[72:76], "little") + offset_difference).to_bytes(4, 'little')
+        for intbyte in range(4):
+            patched_file_data[72+intbyte] = new_xml_size_bytes[intbyte]
+            patched_file_data[76+intbyte] = new_xml_size_bytes[intbyte]
 
     # Write patched model into target
     with open(targetfile + '.pkg', 'wb') as f:
